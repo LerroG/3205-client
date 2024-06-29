b4a5f6bff01c6c5dd1470362ce5e9f418d73998f
Ruslan <ssakoks@mail.ru>
2024-06-29 21:15:38 +0500

    git init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen min-w-screen">
+    <input placeholder="Поиск по Email" class="border rounded-md p-2 outline-none focus:border-gray-400"
+      type="text" />
+  </div>
+</template>
